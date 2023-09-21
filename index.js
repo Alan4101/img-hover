b65a6322eca6936687b4284639c35a006ba2bff2
Alan4101 <andriy410tolochkevitch@gmail.com>
2023-09-22 02:54:11 +0300

    feat: refactor

--- index.js
+++ index.js
@@ -16,46 +16,46 @@ const app = new PIXI.Application({
   sprite.height = canvas.height;
   app.stage.addChild(sprite);
   // Створення shader
-  const shaderCode = `
-  #ifdef GL_ES
-  precision mediump float;
-  #endif
-  // Uniforms from Javascript
-  uniform vec2 uResolution;
-  uniform float uPointerDown;
-  // The texture is defined by PixiJS
-  varying vec2 vTextureCoord;
-  uniform sampler2D uSampler;
-  uniform float iTime; 
-  // Function used to get the distortion effect
-  vec2 computeUV (vec2 uv, float k, float kcube) {
-    vec2 t = uv - 0.5;
-    float r2 = t.x * t.x + t.y * t.y;
-    float f = 0.0;
-    if (kcube == 0.0) {
-      f = 1.0 + r2 * k;
-    } else {
-      f = 1.0 + r2 * (k + kcube * sqrt(r2));
+    const shaderCode = `
+    #ifdef GL_ES
+    precision mediump float;
+    #endif
+    // Uniforms from Javascript
+    uniform vec2 uResolution;
+    uniform float uPointerDown;
+    // The texture is defined by PixiJS
+    varying vec2 vTextureCoord;
+    uniform sampler2D uSampler;
+    uniform float iTime; 
+    // Function used to get the distortion effect
+    vec2 computeUV (vec2 uv, float k, float kcube) {
+      vec2 t = uv - 0.5;
+      float r2 = t.x * t.x + t.y * t.y;
+      float f = 0.0;
+      if (kcube == 0.0) {
+        f = 1.0 + r2 * k;
+      } else {
+        f = 1.0 + r2 * (k + kcube * sqrt(r2));
+      }
+      vec2 nUv = f * t + 0.5;
+      nUv.y = 1.0 - nUv.y;
+      return nUv;
     }
-    vec2 nUv = f * t + 0.5;
-    nUv.y = 1.0 - nUv.y;
-    return nUv;
-  }
-  void main () {
-    // Normalized coordinates
-    vec2 uv = gl_FragCoord.xy / uResolution.xy;
-    
-    float k = -1.0 * sin(uPointerDown * 0.9);
-    float kcube = 0.5 * sin(uPointerDown);
-    float offset = 0.02 * sin(uPointerDown * 0.5);
-    
-    float red = texture2D( uSampler, computeUV( uv, k + offset, kcube ) ).r; 
-    float green = texture2D( uSampler, computeUV( uv, k, kcube ) ).g; 
-    float blue = texture2D( uSampler, computeUV( uv, k - offset, kcube ) ).b; 
-    
-    gl_FragColor = vec4( red, green,blue, 1.0 );
-  }
-  `;
+    void main () {
+      // Normalized coordinates
+      vec2 uv = gl_FragCoord.xy / uResolution.xy;
+      
+      float k = -1.0 * sin(uPointerDown * 0.9);
+      float kcube = 0.5 * sin(uPointerDown);
+      float offset = 0.02 * sin(uPointerDown * 0.5);
+      
+      float red = texture2D( uSampler, computeUV( uv, k + offset, kcube ) ).r; 
+      float green = texture2D( uSampler, computeUV( uv, k, kcube ) ).g; 
+      float blue = texture2D( uSampler, computeUV( uv, k - offset, kcube ) ).b; 
+      
+      gl_FragColor = vec4( red, green,blue, 1.0 );
+    }
+    `;
 
   // Set dimensions
   function initDimensions() {
